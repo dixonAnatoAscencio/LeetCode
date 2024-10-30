7bc86a91450dfa9545e76775b329e5d8c13b2e0e
Dixon <70873819+dixonAnatoAscencio@users.noreply.github.com>
2024-10-29 19:57:54 -0500

    Update ConstainsDuplicates.py

--- ConstainsDuplicates.py
+++ ConstainsDuplicates.py
@@ -1,1 +1,16 @@
 
+#Duplicate Values:
+
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+#Problem: check list for duplicates
+#Solution: Time and Space: O(N): Iterage through list and create set. Space = size of set
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) == len(nums):
+            return False
+        else:
+            return True
+
+#[1,2,3,1] true
